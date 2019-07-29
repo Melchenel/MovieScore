9c70785e764f0a7e5ec90ff6756a05bed959209a
Анна <anna@MacBook-Pro-Anna.local>
2019-07-29 13:01:55 +0300

    Добавлены связи между таблицами User и Message

--- src/main/java/com/example/moviescore/config/WebSecurityConfig.java
+++ src/main/java/com/example/moviescore/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.moviescore.config;
 
 
+import com.example.moviescore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,7 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private DataSource dataSource;
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -51,11 +52,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password, active from usr where username = ?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where username = ?");
-    }
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+                   }
 }
 
